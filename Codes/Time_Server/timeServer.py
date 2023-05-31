b2aa33d6c9ab0408199e7dde8da2e73138e3f316
Ahmed Ibrahem <ahmedibrahemabdelhamid@gmail.com>
2023-05-31 09:49:07 +0300

    add files.

---
+++ Codes/Time_Server/timeServer.py
@@ -0,0 +1,15 @@
+import socket
+import datetime
+
+S = socket.socket(socket.AF_INET, socket. SOCK_STREAM)
+S.bind((socket.gethostname(), 1025))
+S.listen(5)
+
+dateAsString = str(datetime.datetime.now())
+x = dateAsString.encode()
+
+while True:
+    clt, adr = S.accept()
+    print(f"Connection to {adr} is established")
+    clt.send(x)
+# Since bytes is used utf-8 tells what kind of byte is used
